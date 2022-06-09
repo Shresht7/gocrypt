aa72457b7e33a641e03c9252930ffeb4e6e8c41b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-06-09 15:43:17 +0530

    Add tests for xchacha20poly1305

--- encryption/xchacha20ploy1305/xchacha20poly1305.go
+++ encryption/xchacha20ploy1305/xchacha20poly1305.go
@@ -2,24 +2,31 @@
 package xchacha20poly1305
 
 import (
-	"github.com/Shresht7/gocrypt/key/argon2id"
+	"github.com/Shresht7/gocrypt/hash"
 	"github.com/Shresht7/gocrypt/utils"
 	"golang.org/x/crypto/chacha20poly1305"
 )
 
+//	Generates a new key from the given secret of the given size using argon2id.
+//	Panics if the key generation fails.
+func GenerateKey(secret []byte) []byte {
+	key, err := hash.HMAC_SHA_512_256(secret, string(secret))
+	if err != nil {
+		panic(err) //	Key generation is critical
+	}
+	return key
+}
+
 //	Encrypt the given text
-func Encrypt(text, secret string) (string, error) {
+func Encrypt(text, secret []byte) ([]byte, error) {
 
 	//	Generate Key from argon2id
-	key, err := argon2id.Hash([]byte(secret), argon2id.DefaultParams)
-	if err != nil {
-		return "", err
-	}
+	key := GenerateKey(secret)
 
 	//	Generate Cipher Block
 	aead, err := chacha20poly1305.NewX(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	plaintext := []byte(text)
@@ -27,26 +34,24 @@ func Encrypt(text, secret string) (string, error) {
 	//	Generate Nonce
 	nonce, err := utils.GenerateBytes(aead.NonceSize(), aead.NonceSize()+len(plaintext)+aead.Overhead())
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	//	Seal the text
 	ciphertext := aead.Seal(nonce, nonce, plaintext, nil)
 
-	return utils.EncodeBase64(ciphertext), nil
+	return ciphertext, nil
 }
 
-func Decrypt(text, secret string) (string, error) {
+//	Decrypt the given ciphertext
+func Decrypt(text, secret []byte) ([]byte, error) {
 
 	//	Generate Key from argon2id
-	key, err := argon2id.Hash([]byte(secret), argon2id.DefaultParams)
-	if err != nil {
-		return "", err
-	}
+	key := GenerateKey(secret)
 
 	aead, err := chacha20poly1305.NewX(key)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	ciphertext := []byte(text)
@@ -54,14 +59,14 @@ func Decrypt(text, secret string) (string, error) {
 	//	Split ciphertext into nonce and ciphertext
 	nonce, ciphertext := ciphertext[:aead.NonceSize()], ciphertext[aead.NonceSize():]
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
 	//	Decrypt
 	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 
-	return string(plaintext), nil
+	return plaintext, nil
 }
--- encryption/xchacha20ploy1305/xchacha20poly1305_test.go
+++ encryption/xchacha20ploy1305/xchacha20poly1305_test.go
@@ -1,4 +1,4 @@
-package aes_256_gcm
+package xchacha20poly1305
 
 import "testing"
 
@@ -9,7 +9,7 @@ var (
 
 //	Tests the key generation
 func TestGenerateKey(t *testing.T) {
-
+	
 	key := GenerateKey(secret)
 	if len(key) != 32 {
 		t.Error("key length should be 32")
@@ -19,17 +19,17 @@ func TestGenerateKey(t *testing.T) {
 
 //	Test the encrypt / decrypt process
 func TestEncryptDecrypt(t *testing.T) {
-
+	
 	ciphertext, err := Encrypt(text, secret)
 	if err != nil {
 		t.Error(err)
 	}
-
+	
 	plaintext, err := Decrypt(ciphertext, secret)
 	if err != nil {
 		t.Error(err)
 	}
-
+	
 	if string(text) != string(plaintext) {
 		t.Error("plaintexts do not match")
 	}
@@ -38,20 +38,20 @@ func TestEncryptDecrypt(t *testing.T) {
 
 //	Test that decrypting a malformed ciphertext does not produce a plaintext
 func TestMalformedCiphertext(t *testing.T) {
-
+	
 	ciphertext, err := Encrypt(text, secret)
 	if err != nil {
 		t.Error(err)
 	}
-
+	
 	//	Malform ciphertext
 	ciphertext[0] ^= 0xff
-
+	
 	plaintext, err := Decrypt(ciphertext, secret)
 	if err == nil {
 		t.Error("decryption should have failed")
 	}
-
+	
 	//	Plaintexts should not match
 	if string(text) == string(plaintext) {
 		t.Error("plaintexts should not match")
@@ -59,38 +59,38 @@ func TestMalformedCiphertext(t *testing.T) {
 
 }
 
-//	Tests that different secrets produce different ciphertexts
+//	Test that different secrets produce different ciphertexts
 func TestDifferentSecrets(t *testing.T) {
-
+	
 	ciphertext1, err := Encrypt(text, secret)
 	if err != nil {
 		t.Error(err)
 	}
-
+	
 	ciphertext2, err := Encrypt(text, []byte("different secret"))
 	if err != nil {
 		t.Error(err)
 	}
-
+	
 	if string(ciphertext1) == string(ciphertext2) {
 		t.Error("ciphertexts should not match")
 	}
 
 }
 
-//	Test that different plaintexts produce different ciphertexts
-func TestDifferentPlaintext(t *testing.T) {
-
+//	Tests that different plaintexts produce different ciphertexts
+func TestDifferentPlaintexts(t *testing.T) {
+	
 	ciphertext1, err := Encrypt(text, secret)
 	if err != nil {
 		t.Error(err)
 	}
-
+	
 	ciphertext2, err := Encrypt([]byte("different plaintext"), secret)
 	if err != nil {
 		t.Error(err)
 	}
-
+	
 	if string(ciphertext1) == string(ciphertext2) {
 		t.Error("ciphertexts should not match")
 	}
@@ -99,7 +99,7 @@ func TestDifferentPlaintext(t *testing.T) {
 
 //	Test that a different secret does not decrypt a ciphertext
 func TestSecretDecrypt(t *testing.T) {
-
+	
 	ciphertext, err := Encrypt(text, secret)
 	if err != nil {
 		t.Error(err)
@@ -113,5 +113,5 @@ func TestSecretDecrypt(t *testing.T) {
 	if string(text) == string(plaintext) {
 		t.Error("plaintexts should not match")
 	}
-
+	
 }
